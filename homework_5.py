cc1dfae44ad0206dab57c4e9a26546c83be459ae
Victor <gorovojviktor@gmail.com>
2017-04-25 23:43:22 +0300

    18-20

---
+++ homework_5.py
@@ -0,0 +1,34 @@
+import math
+# 18
+
+# def summ_unicode(first, second):
+#     for n in range(first, second + 1):
+#         second = n + first
+#     return second
+#
+# symbol_1 = ord(input("Enter symbol 1:"))
+# symbol_2 = ord(input("Enter symbol 2:"))
+#
+# #debug for user
+# print(symbol_1)
+# print(symbol_2)
+# #
+#
+# print(summ_unicode(symbol_1, symbol_2))
+
+
+# 19
+
+# sum_of_all_numbers = 0
+# for degree in range(int(math.pow(1000000, 1 / 3)) + 1):
+#     degree = degree ** 3
+#     sum_of_all_numbers = sum_of_all_numbers + degree
+#
+# print("The sum of all numbers, the degree of 3:", sum_of_all_numbers)
+
+# 20
+
+# for x in range(1000):
+#     if '1' in str(x) and '7' in str(x):
+#         print(x)
+

9daa6e165575b1d650b3582246926cb317d3bac2
Victor <gorovojviktor@gmail.com>
2017-05-01 17:15:40 +0300

    Edit 18 and 20 task

--- homework_5.py
+++ homework_5.py
@@ -2,33 +2,42 @@ import math
 # 18
 
 # def summ_unicode(first, second):
-#     for n in range(first, second + 1):
-#         second = n + first
-#     return second
 #
-# symbol_1 = ord(input("Enter symbol 1:"))
-# symbol_2 = ord(input("Enter symbol 2:"))
+#     max_elemment = max(ord(first), ord(second))
+#     min_elemment = min(ord(first), ord(second))
 #
-# #debug for user
-# print(symbol_1)
-# print(symbol_2)
-# #
+#     summ_unic = 0
+#
+#     for n in range(min_elemment, max_elemment + 1):
+#         summ_unic = n + summ_unic
+#     return summ_unic
+#
+# symbol_1 = str(input("Enter symbol 1: "))
+# symbol_2 = str(input("Enter symbol 2: "))
 #
 # print(summ_unicode(symbol_1, symbol_2))
 
 
 # 19
 
-# sum_of_all_numbers = 0
-# for degree in range(int(math.pow(1000000, 1 / 3)) + 1):
-#     degree = degree ** 3
-#     sum_of_all_numbers = sum_of_all_numbers + degree
+# def sum_of_all_numbers(number):
 #
-# print("The sum of all numbers, the degree of 3:", sum_of_all_numbers)
+#     sum_of_number = 0
+#
+#     for degree in range(int(math.pow(1000000, 1 / number)) + 1):
+#         degree = degree ** 3
+#         sum_of_number = sum_of_number + degree
+#
+#     print("The sum of all numbers that are a power of:", sum_of_number)
+#
+# sum_of_all_numbers(3)
 
 # 20
+def find_number(number_1, number_2):
 
-# for x in range(1000):
-#     if '1' in str(x) and '7' in str(x):
-#         print(x)
+    for x in range(1000):
 
+        if number_1 in str(x) and number_2 in str(x):
+            print(x)
+
+find_number('1', '7')

28bf99b0f3980c2348acae07b79e00defcd78929
Victor <gorovojviktor@gmail.com>
2017-05-17 14:45:37 +0300

    Edit 27-30

--- homework_7.py
+++ homework_7.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 # 25
 # def average(low_random, high_random):
@@ -48,51 +48,111 @@ import random
 # for i in range(50):
 #     if i % 2 != 0:
 #         lists.append(i)
+#
 # print(lists)
 #
-# a = random.shuffle(lists)
+# for i in range(len(lists) - 1):
+#     j = random.randint(i + 1, len(lists) - 1)
+#     x = lists[i]
+#     lists[i] = lists[j]
+#     lists[j] = x
+#
 # print(lists)
+
+# Function create random list
+
+def randomize_list(num, lower_limit, upper_limit):
+    arrays = []
+    for i in range(num):
+        arrays.append(randint(lower_limit, upper_limit))
+    return arrays
 
 # 28
 
-# lists_1 = []
-# lists_2 = []
+# def avr_list(array):
+#     sum = 0
+#     for elem in array:
+#         sum += elem
+#     return sum/len(array)
 #
-# for i in range(0, 5 + 1):
-#     item_1 = random.randint(0, 5 + 1)
-#     item_2 = random.randint(0, 5 + 1)
+# def comparison(arr_1, arr_2):
+#     if avr_list(arr_1) == avr_list(arr_2):
+#         print("The average arithmetic lists are equal")
+#     elif avr_list(arr_1) > avr_list(arr_2):
+#         print("The arithmetic average of List 1 is greater")
+#     elif avr_list(arr_1) < avr_list(arr_2):
+#         print("The arithmetic average of List 2 is greater")
 #
-#     lists_1.append(item_1)
-#     lists_2.append(item_2)
+# array_1 = randomize_list(5, 0, 5)
+# print("First array", array_1)
 #
-# # output values
-# print(lists_1, end="\n")
-# print(lists_2)
+# array_2 = randomize_list(5, 0, 5)
+# print("Second array", array_2)
 #
-# def sum_list(lst):
+# print('AVR first list =', avr_list(array_1))
+# print('AVR second list =', avr_list(array_2))
 #
-#     sum_lst = 0
-#
-#     for i in lst:
-#         sum_lst += i
-#     return sum_lst
-#
-# sum_list_1 = sum_list(lists_1)
-# sum_list_2 = sum_list(lists_2)
-#
-# avr_1 = sum_list_1 / len(lists_1)
-# avr_2 = sum_list_2 / len(lists_2)
-#
-# if sum_list_1 > sum_list_2:
-#     print("Avr first list is greater")
-# elif sum_list_1 < sum_list_2:
-#     print("Avr second list is greater")
-# else: print("Avr are equal")
+# comparison(array_1, array_2)
 
 
 # 29
 
+# def frequency(lst):
+#     count0 = 0
+#     count1 = 0
+#     count2 = 0
+#
+#     for elem in lst:
+#         if elem == 0:
+#             count0 += 1
+#             continue
+#         elif elem == 1:
+#             count1 += 1
+#             continue
+#         elif elem == -1:
+#             count2 += 1
+#
+#     if count0 > max(count1, count2):
+#         print("The number 0 (zero) occurs most often:", count0)
+#     elif count1 > max(count0, count2):
+#         print("The number 1 (one) occurs most often:", count1)
+#     elif count2 > max(count0, count1):
+#         print("The number -1 (minus one) occurs most often:", count2)
+#     else: print("Multiple numbers are repeated an equal number of times")
+#
+# lst = randomize_list(11, -1, 1)
+# print(lst)
+# frequency(lst)
 
+# 30
 
+# def string2list(line): #Перевод строки в список
+#     lists = []
+#     for i in range(len(line)):
+#         lists.append(line[i])
+#     return lists
+#
+# def encryption(user_line):
+#
+#     encrypt_text = ''
+#
+#     for i in range(len(user_line)):
+#         id_char = encrypt.find(text[i])
+#         if id_char + key < 0:
+#             user_line[i] = encrypt[len(encrypt) + id_char + key]
+#         elif id_char + key >= len(encrypt):
+#             user_line[i] = encrypt[id_char - len(encrypt) + key]
+#         else: user_line[i] = encrypt[id_char + key]
+#
+#     for i in range(len(user_line)):
+#         encrypt_text += user_line[i]
+#     print(encrypt_text)
+#
+# key = 5
+# encrypt = "qwertyuioppasdfghjkl\'\\zxcvbnm1234567890QWERTYUIOPAASDFGHJKLZXCVBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю"
+#
+# text = input("Enter text")
+# user_list = string2list(text)
+# encryption(user_list)
 
 

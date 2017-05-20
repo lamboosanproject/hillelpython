9ff2c23be7deb948e49906ab109b67b1f11705d9
Victor <gorovojviktor@gmail.com>
2017-05-20 22:44:23 +0300

    Edit generator

--- homework_8.py
+++ homework_8.py
@@ -1,13 +1,33 @@
-from random import choice, randint
+
 
 # 31
-length = int(input('Enter length password: '))
 
-char = list('_123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
+def pwd_gen(length = 8):
+    from random import choice
+    char = list('_123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
 
-print(''.join([choice(char) for x in range(length)]))
+    return ''.join([choice(char) for x in range(length)])
 
+def test_pwd():
+    import string
+    tries = 0
 
+    while tries < 1000:
+        pwd = pwd_gen(8)
 
+        comps = [0, 0, 0]
+        for c in pwd:
+            comps[0] |= c in string.ascii_lowercase
+            comps[1] |= c in string.ascii_uppercase
+            comps[2] |= c in string.digits
 
+        if sum(comps) < 3:
+            print("Ops, something went wrong: ", pwd)
+            break
+        else:
+            print("OK: ", pwd)
+
+        tries += 1
+
+test_pwd()
 

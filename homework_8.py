dd2a712c42fd723f2b40c92cf1f2b83ce2c796c4
Victor <gorovojviktor@gmail.com>
2017-05-27 14:52:27 +0300

    Edit 31

--- homework_8.py
+++ homework_8.py
@@ -1,19 +1,16 @@
-
-
 # 31
-
 def pwd_gen(length = 8):
     from random import choice
     char = list('_123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
 
     return ''.join([choice(char) for x in range(length)])
 
-def test_pwd():
+def pwd_verification():
     import string
     tries = 0
 
     while tries < 1000:
-        pwd = pwd_gen(8)
+        pwd = pwd_gen(12)
 
         comps = [0, 0, 0]
         for c in pwd:
@@ -22,12 +19,12 @@ def test_pwd():
             comps[2] |= c in string.digits
 
         if sum(comps) < 3:
-            print("Ops, something went wrong: ", pwd)
+            print("Dann leer")
             break
         else:
             print("OK: ", pwd)
 
         tries += 1
 
-test_pwd()
+pwd_verification()
 
